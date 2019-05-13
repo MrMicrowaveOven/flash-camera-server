b881b936dc26bfe46c85bced634c16791dca3ed1
Benjamin Zagorski <benj@zagorski.com>
2019-05-13 15:33:00 -0700

    Created basic server, which uploads image to s3 with unique name, and returns the link to the image

---
+++ helloWorld.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+# import logging
+import boto3
+import random
+
+import time
+
+# from botocore.exceptions import ClientError
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return uploadImageToS3()
+def uploadImageToS3():
+    s3_client = boto3.client('s3')
+    bucket_name = 'flash-images-store'
+    time_num = str(int(time.time()))
+    bucket_file_name = 'marilyn' + time_num + '.jpg'
+    s3_client.upload_file('marilyn.jpg', bucket_name, bucket_file_name, ExtraArgs={'ContentType': 'image/jpeg'})
+    return 'https://s3-us-west-1.amazonaws.com/flash-images-store/' + bucket_file_name
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0')
