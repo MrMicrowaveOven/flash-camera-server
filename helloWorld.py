a62278702ae4d9e692c5b48b433772b9cf20a40d
Ksenia Kriksunov <kithaca@gmail.com>
2019-05-13 23:06:28 -0700

    minor cosmetic suggestions!
    Notes:
    - Following PEP-8 guidelines for order of imports and for added blank lines
    - Changed string construction to use string interpolation
    - Blank line before return for readability

--- helloWorld.py
+++ helloWorld.py
@@ -1,16 +1,23 @@
-from flask import Flask
-
-import boto3
 import time
 
+import boto3
+
+from flask import Flask
+
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     s3_client = boto3.client('s3')
     bucket_name = 'flash-images-store'
-    time_num = str(int(time.time()))
-    bucket_file_name = 'marilyn' + time_num + '.jpg'
+    time_num = int(time.time())
+    bucket_file_name = 'marilyn%s.jpg' % time_num
     s3_client.upload_file('marilyn.jpg', bucket_name, bucket_file_name, ExtraArgs={'ContentType': 'image/jpeg'})
-    return 'https://s3-us-west-1.amazonaws.com/flash-images-store/' + bucket_file_name
+
+    return 'https://s3-us-west-1.amazonaws.com/%s/%s' % (bucket_name, bucket_file_name)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
