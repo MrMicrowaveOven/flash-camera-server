f57abd6d856ddab3ad8c15e800e182c82f7eddbe
Benjamin Zagorski <benj@zagorski.com>
2019-05-15 19:31:02 -0700

    Now takes picture

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,5 @@
 import time
+from time import sleep
 
 import boto3
 
@@ -10,13 +11,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    file_name = take_picture()
+    return send_picture_to_s3(file_name)
+
+def take_picture():
+    from picamera import PiCamera
+
+    time_num = int(time.time())
+    file_name = 'chateau%s.jpg' % time_num
+
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    sleep(2)
+    camera.capture(file_name)
+    camera.close()
+
+    print file_name
+    return file_name
+def send_picture_to_s3(file_name):
     s3_client = boto3.client('s3')
     bucket_name = 'flash-images-store'
-    time_num = int(time.time())
-    bucket_file_name = 'marilyn%s.jpg' % time_num
-    s3_client.upload_file('marilyn.jpg', bucket_name, bucket_file_name, ExtraArgs={'ContentType': 'image/jpeg'})
 
-    return 'https://s3-us-west-1.amazonaws.com/%s/%s' % (bucket_name, bucket_file_name)
+    s3_client.upload_file(file_name, bucket_name, file_name, ExtraArgs={'ContentType': 'image/jpeg'})
+
+    return 'https://s3-us-west-1.amazonaws.com/%s/%s' % (bucket_name, file_name)
 
 
 if __name__ == '__main__':
