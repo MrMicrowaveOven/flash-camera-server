6f1d5433a713849ec2dc1920c1d096788952db05
mrmicrowaveoven <benj@zagorski.com>
2020-02-10 19:45:12 -0800

    Moved heartbeat method to bottom, so other methods will be defined

--- app.py
+++ app.py
@@ -16,23 +16,26 @@ mothership_url = 'https://flash-sms-server.herokuapp.com'
 camera_url = mothership_url + '/cameras/' +  str(camera_name.camera_id)
 camera_name_str = camera_name.camera_name
 
-while True:
-    response = requests.get(camera_url)
+def start_heartbeat():
+    while True:
+        response = requests.get(camera_url)
 
-    picture_id = response.json()['picture_id']
-    if picture_id:
-        # Take a picture
-        file_name = take_picture()
-        # Upload to S3
-        photo_url = send_picture_to_s3(file_name)
-        # Delete local photo
-        delete_image(file_name)
+        print(response)
+        print(response.json())
+        picture_id = response.json()['picture_id']
+        if picture_id:
+            # Take a picture
+            file_name = take_picture()
+            # Upload to S3
+            photo_url = send_picture_to_s3(file_name)
+            # Delete local photo
+            delete_image(file_name)
 
-        requests.patch(mothership_url + '/pictures/' + str(picture_id), {'photo_url': photo_url})
-        print('updating')
-    else:
-        print('standing by')
-        sleep(1)
+            requests.patch(mothership_url + '/pictures/' + str(picture_id), {'photo_url': photo_url})
+            print('updating')
+        else:
+            print('standing by')
+            sleep(1)
 
 def send_picture_to_s3(file_name):
     s3_client = boto3.client('s3')
@@ -54,3 +57,5 @@ def delete_image(file_name):
     os.remove(file_name)
     print("File removed!")
     print(file_name)
+
+start_heartbeat()
