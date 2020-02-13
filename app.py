764af57dd1add5d28ea2e86a6c5a7285673362ae
mrmicrowaveoven <benj@zagorski.com>
2020-02-12 17:48:59 -0800

    Logs picture_id instead of file_name

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def start_heartbeat():
 
 
             if len(current_message_log) == 0:
-                print('sending picture' + file_name)
+                print('sending picture ' + picture_id)
         elif len(current_message_log) == 0:
             current_message_log.append('standing by')
 
