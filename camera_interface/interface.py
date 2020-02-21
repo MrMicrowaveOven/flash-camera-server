44861924e43889dab6667cef57f6ac64c7908cb9
Benjamin Zagorski <benj@zagorski.com>
2020-02-20 21:10:44 -1000

    Interface is now centered

--- camera_interface/interface.py
+++ camera_interface/interface.py
@@ -15,7 +15,7 @@ window = Tk()
 window.title("Admin Control Panel")
 
 window.attributes('-fullscreen', True)
-welcome_lbl = Label(window, text="Hello!  Welcome to the Administrative Control Panel.")
+welcome_lbl = Label(window, text="Hello!  Welcome to the Administrative Control Panel.", font=(20))
 
 def remove_this_network(i):
 	if tkinter.messagebox.askokcancel("Confirm Network Deletion","Would you like to remove the network " + wifi_networks[i] + " from your list of known networks?"):
@@ -77,29 +77,30 @@ current_wifis_lbl = Label(window, text="Current saved wifi networks:")
 
 wifi_networks = wifi_info_module.get_wifi_list()
 for i in range(len(wifi_networks)):
-	Label(window, text=wifi_networks[i]).grid(column=BASE_COLUMN+0, row=(101 + i))
-
-update_button = Button(window, text="Update Device", command=update_flash_cam_repo)
+	y_coord = 0.25 + (0.025 * i)
+	Label(window, text=wifi_networks[i]).place(relx = 0.5, rely = y_coord, anchor = CENTER)
 
 reset_button = Button(window, text="Reset Flash-Cam", command=confirm_reset)
+
+update_button = Button(window, text="Update Device via Network", command=update_flash_cam_repo)
 
 camera_preview_button = Button(window, text="Preview Camera for 10 seconds", command=preview_camera)
 
 programmer_mode_button = Button(window, text="Programmer Mode", command=window.destroy)
 programmer_mode_warning_label = Label(window, text="(please don't enter Programmer Mode unless you really know what you're doing)")
 
-welcome_lbl.grid(column=BASE_COLUMN, row=0)
-wifi_lbl.grid(column=BASE_COLUMN, row=1)
-wifi_name_lbl.grid(column=0, row=2)
-wifi_name_field.grid(column=BASE_COLUMN+1, row=2)
-wifi_password_lbl.grid(column=0, row=3)
-wifi_password_field.grid(column=BASE_COLUMN+1, row=3)
-add_wifi_button.grid(column=2, row=3)
-current_wifis_lbl.grid(column=BASE_COLUMN+0, row=100)
-update_button.grid(column=BASE_COLUMN, row=500)
-reset_button.grid(column=BASE_COLUMN, row=1000)
-camera_preview_button.grid(column=BASE_COLUMN, row=1500)
-programmer_mode_button.grid(column=BASE_COLUMN, row=2500)
-programmer_mode_warning_label.grid(column=0, row=2600)
+welcome_lbl.place(relx = 0.5, rely = 0.2, anchor = CENTER)
+current_wifis_lbl.place(relx = 0.5, rely = 0.225, anchor = CENTER)
+wifi_lbl.place(relx = 0.5, rely = 0.4, anchor = CENTER)
+wifi_name_lbl.place(relx = 0.5, rely = 0.425, anchor = CENTER)
+wifi_name_field.place(relx = 0.5, rely = 0.45, anchor = CENTER)
+wifi_password_lbl.place(relx = 0.5, rely = 0.475, anchor = CENTER)
+wifi_password_field.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+add_wifi_button.place(relx = 0.5, rely = 0.53, anchor = CENTER)
+reset_button.place(relx = 0.5, rely = 0.7, anchor = CENTER)
+update_button.place(relx = 0.5, rely = 0.6, anchor = CENTER)
+camera_preview_button.place(relx = 0.5, rely = 0.65, anchor = CENTER)
+programmer_mode_button.place(relx = 0.5, rely = 0.75, anchor = CENTER)
+programmer_mode_warning_label.place(relx = 0.5, rely = 0.775, anchor = CENTER)
 
 window.mainloop()
