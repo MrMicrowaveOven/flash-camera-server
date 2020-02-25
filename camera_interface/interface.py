483c2a297c02dde798a180aeb71f3becdcf6cd96
mrmicrowaveoven <benj@zagorski.com>
2020-02-24 19:22:40 -0800

    Moved all interface code to launch_interface() method

--- camera_interface/interface.py
+++ camera_interface/interface.py
@@ -1,141 +1,143 @@
-from tkinter import *
-import tkinter.messagebox
+def launch_interface():
+	from tkinter import *
+	import tkinter.messagebox
 
-import git
+	import git
 
-import wifi_info_module
+	import wifi_info_module
 
-from picamera import PiCamera
-import time
+	from picamera import PiCamera
+	import time
 
-BASE_COLUMN = 0
+	BASE_COLUMN = 0
 
-window = Tk()
+	window = Tk()
 
-window.title("Admin Control Panel")
+	window.title("Admin Control Panel")
 
-window.attributes('-fullscreen', True)
-welcome_lbl = Label(window, text="Hello!  Welcome to the Administrative Control Panel.", font=(20))
+	window.attributes('-fullscreen', True)
+	welcome_lbl = Label(window, text="Hello!  Welcome to the Administrative Control Panel.", font=(20))
 
-network_list_labels = []
-network_removal_buttons = []
-
-wifi_networks = wifi_info_module.get_wifi_list()
-
-def remove_this_network(network_name):
-	if tkinter.messagebox.askokcancel("Confirm Network Deletion","Would you like to remove the network " + network_name + " from your list of known networks?"):
-		print("REMOVING " + network_name)
-		wifi_info_module.remove_network(network_name)
-		# update_network_list()
-
-def update_network_list():
-	# hide_removal_buttons()
-	global network_removal_buttons
-	global network_list_labels
-	global wifi_networks
-	wifi_networks = wifi_info_module.get_wifi_list()
-	for label in network_list_labels:
-		label.pack_forget()
 	network_list_labels = []
-
-	for i in range(len(wifi_networks)):
-		y_coord = 0.25 + (0.025 * i)
-		wifi_label = Label(window, text=wifi_networks[i])
-		wifi_label.place(relx = 0.5, rely = y_coord, anchor = CENTER)
-		network_list_labels.append(wifi_label)
-
-def show_removal_buttons():
-	network_removal_buttons = []
-	global wifi_networks
-	for i in range(len(wifi_networks)):
-		y_coord = 0.25 + (0.025 * i)
-		removal_button = Button(window, text="X", command= lambda i=i: remove_this_network(wifi_networks[i]))
-		removal_button.place(relx = 0.4, rely = y_coord, anchor = CENTER)
-		network_removal_buttons.append(removal_button)
-
-def hide_removal_buttons():
-	global network_removal_buttons
-	for button in network_removal_buttons:
-		button.pack_forget()
 	network_removal_buttons = []
 
-def update_flash_cam_repo():
-	g = git.cmd.Git('/home/pi/Desktop/flash-camera-server/')
-	try:
-		pull_response = g.pull()
-	except Exception as e:
-		print(e)
-		pull_response_label = Label(window, bg="orange", text="There was an issue updating.  Please check your internet connection and try again.")
+	wifi_networks = wifi_info_module.get_wifi_list()
+
+	def remove_this_network(network_name):
+		if tkinter.messagebox.askokcancel("Confirm Network Deletion","Would you like to remove the network " + network_name + " from your list of known networks?"):
+			print("REMOVING " + network_name)
+			wifi_info_module.remove_network(network_name)
+			# update_network_list()
+
+	def update_network_list():
+		# hide_removal_buttons()
+		global network_removal_buttons
+		global network_list_labels
+		global wifi_networks
+		wifi_networks = wifi_info_module.get_wifi_list()
+		for label in network_list_labels:
+			label.pack_forget()
+		network_list_labels = []
+
+		for i in range(len(wifi_networks)):
+			y_coord = 0.25 + (0.025 * i)
+			wifi_label = Label(window, text=wifi_networks[i])
+			wifi_label.place(relx = 0.5, rely = y_coord, anchor = CENTER)
+			network_list_labels.append(wifi_label)
+
+	def show_removal_buttons():
+		network_removal_buttons = []
+		global wifi_networks
+		for i in range(len(wifi_networks)):
+			y_coord = 0.25 + (0.025 * i)
+			removal_button = Button(window, text="X", command= lambda i=i: remove_this_network(wifi_networks[i]))
+			removal_button.place(relx = 0.4, rely = y_coord, anchor = CENTER)
+			network_removal_buttons.append(removal_button)
+
+	def hide_removal_buttons():
+		global network_removal_buttons
+		for button in network_removal_buttons:
+			button.pack_forget()
+		network_removal_buttons = []
+
+	def update_flash_cam_repo():
+		g = git.cmd.Git('/home/pi/Desktop/flash-camera-server/')
+		try:
+			pull_response = g.pull()
+		except Exception as e:
+			print(e)
+			pull_response_label = Label(window, bg="orange", text="There was an issue updating.  Please check your internet connection and try again.")
+			pull_response_label.place(relx = 0.5, rely = 0.625, anchor = CENTER)
+			return()
+		print(pull_response)
+		pull_response_label = ''
+		if pull_response == "Already up-to-date.":
+			pull_response_label = Label(window, bg="orange", text=pull_response)
+		else:
+			pull_response_label = Label(window, bg="orange", text="Updated!  You'll have the most recent version after you restart.")
 		pull_response_label.place(relx = 0.5, rely = 0.625, anchor = CENTER)
-		return()
-	print(pull_response)
-	pull_response_label = ''
-	if pull_response == "Already up-to-date.":
-		pull_response_label = Label(window, bg="orange", text=pull_response)
-	else:
-		pull_response_label = Label(window, bg="orange", text="Updated!  You'll have the most recent version after you restart.")
-	pull_response_label.place(relx = 0.5, rely = 0.625, anchor = CENTER)
 
-def confirm_reset():
-	if tkinter.messagebox.askokcancel("Reset Camera?","Are you sure you would like to reset your camera?"):
-		reset_camera()
+	def confirm_reset():
+		if tkinter.messagebox.askokcancel("Reset Camera?","Are you sure you would like to reset your camera?"):
+			reset_camera()
 
-def reset_camera():
-	command = "/usr/bin/sudo /sbin/shutdown -r now"
-	import subprocess
-	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-	output = process.communicate()[0]
-	print(output)
+	def reset_camera():
+		command = "/usr/bin/sudo /sbin/shutdown -r now"
+		import subprocess
+		process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+		output = process.communicate()[0]
+		print(output)
 
-def preview_camera():
-	camera = PiCamera()
-	camera.start_preview()
-	time.sleep(10)
-	camera.close()
+	def preview_camera():
+		camera = PiCamera()
+		camera.start_preview()
+		time.sleep(10)
+		camera.close()
 
-def update_wifi_info():
-	wifi_info_module.update_wifi_info(wifi_name_field.get(), wifi_password_field.get())
-	wifi_confirmation_lbl = Label(window, bg="orange", text="Wifi info for " + wifi_name_field.get() + " added!  Restart to connect!")
-	wifi_confirmation_lbl.place(relx = 0.5, rely = 0.56, anchor = CENTER)
+	def update_wifi_info():
+		wifi_info_module.update_wifi_info(wifi_name_field.get(), wifi_password_field.get())
+		wifi_confirmation_lbl = Label(window, bg="orange", text="Wifi info for " + wifi_name_field.get() + " added!  Restart to connect!")
+		wifi_confirmation_lbl.place(relx = 0.5, rely = 0.56, anchor = CENTER)
 
-wifi_lbl = Label(window, text="Add Wifi Info:")
+	wifi_lbl = Label(window, text="Add Wifi Info:")
 
-wifi_name_lbl = Label(window, text="Wifi Name")
-wifi_name_field = Entry(window)
+	wifi_name_lbl = Label(window, text="Wifi Name")
+	wifi_name_field = Entry(window)
 
-wifi_password_lbl = Label(window, text="Wifi Password")
-wifi_password_field = Entry(window)
+	wifi_password_lbl = Label(window, text="Wifi Password")
+	wifi_password_field = Entry(window)
 
-add_wifi_button = Button(window, text="Add wifi info", command=update_wifi_info)
-show_wifi_remove_buttons_button = Button(window, text="Remove wifi info", command=show_removal_buttons)
+	add_wifi_button = Button(window, text="Add wifi info", command=update_wifi_info)
+	show_wifi_remove_buttons_button = Button(window, text="Remove wifi info", command=show_removal_buttons)
 
-current_wifis_lbl = Label(window, text="Current saved wifi networks:")
+	current_wifis_lbl = Label(window, text="Current saved wifi networks:")
 
-update_network_list()
+	update_network_list()
 
-reset_button = Button(window, text="Reset Flash-Cam", command=confirm_reset)
+	reset_button = Button(window, text="Reset Flash-Cam", command=confirm_reset)
 
-update_button = Button(window, text="Update Device via Network", command=update_flash_cam_repo)
+	update_button = Button(window, text="Update Device via Network", command=update_flash_cam_repo)
 
-camera_preview_button = Button(window, text="Preview Camera for 10 seconds", command=preview_camera)
+	camera_preview_button = Button(window, text="Preview Camera for 10 seconds", command=preview_camera)
 
-programmer_mode_button = Button(window, text="Programmer Mode", command=window.destroy)
-programmer_mode_warning_label = Label(window, text="(please don't enter Programmer Mode unless you really know what you're doing)")
+	programmer_mode_button = Button(window, text="Programmer Mode", command=window.destroy)
+	programmer_mode_warning_label = Label(window, text="(please don't enter Programmer Mode unless you really know what you're doing)")
 
-welcome_lbl.place(relx = 0.5, rely = 0.2, anchor = CENTER)
-current_wifis_lbl.place(relx = 0.5, rely = 0.225, anchor = CENTER)
+	welcome_lbl.place(relx = 0.5, rely = 0.2, anchor = CENTER)
+	current_wifis_lbl.place(relx = 0.5, rely = 0.225, anchor = CENTER)
 
-wifi_lbl.place(relx = 0.5, rely = 0.4, anchor = CENTER)
-wifi_name_lbl.place(relx = 0.5, rely = 0.425, anchor = CENTER)
-wifi_name_field.place(relx = 0.5, rely = 0.45, anchor = CENTER)
-wifi_password_lbl.place(relx = 0.5, rely = 0.475, anchor = CENTER)
-wifi_password_field.place(relx = 0.5, rely = 0.5, anchor = CENTER)
-add_wifi_button.place(relx = 0.4, rely = 0.53, anchor = CENTER)
-show_wifi_remove_buttons_button.place(relx = 0.6, rely = 0.53, anchor = CENTER)
-update_button.place(relx = 0.5, rely = 0.6, anchor = CENTER)
-camera_preview_button.place(relx = 0.5, rely = 0.65, anchor = CENTER)
-reset_button.place(relx = 0.5, rely = 0.7, anchor = CENTER)
-programmer_mode_button.place(relx = 0.5, rely = 0.75, anchor = CENTER)
-programmer_mode_warning_label.place(relx = 0.5, rely = 0.775, anchor = CENTER)
+	wifi_lbl.place(relx = 0.5, rely = 0.4, anchor = CENTER)
+	wifi_name_lbl.place(relx = 0.5, rely = 0.425, anchor = CENTER)
+	wifi_name_field.place(relx = 0.5, rely = 0.45, anchor = CENTER)
+	wifi_password_lbl.place(relx = 0.5, rely = 0.475, anchor = CENTER)
+	wifi_password_field.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+	add_wifi_button.place(relx = 0.4, rely = 0.53, anchor = CENTER)
+	show_wifi_remove_buttons_button.place(relx = 0.6, rely = 0.53, anchor = CENTER)
+	update_button.place(relx = 0.5, rely = 0.6, anchor = CENTER)
+	camera_preview_button.place(relx = 0.5, rely = 0.65, anchor = CENTER)
+	reset_button.place(relx = 0.5, rely = 0.7, anchor = CENTER)
+	programmer_mode_button.place(relx = 0.5, rely = 0.75, anchor = CENTER)
+	programmer_mode_warning_label.place(relx = 0.5, rely = 0.775, anchor = CENTER)
 
-window.mainloop()
+	window.mainloop()
+launch_interface()
